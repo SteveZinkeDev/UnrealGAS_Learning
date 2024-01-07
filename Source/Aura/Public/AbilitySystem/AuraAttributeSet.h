087ef7a0c1872bc91ae5f180b214c35561211bbd
Steve Zinke <70287923+SteveZinkeDev@users.noreply.github.com>
2024-01-07 23:37:16 +0100

    added first attributes with macros; changed console key

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -4,7 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "AuraAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+        GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+        GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+        GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+        GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -12,6 +19,38 @@
 UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
-	GENERATED_BODY()
-	
+  GENERATED_BODY()
+
+  public:
+    UAuraAttributeSet ();
+
+    virtual void GetLifetimeReplicatedProps (TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+    /* Health */
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Main Stats")
+    FGameplayAttributeData MaxHealth;
+    ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth);
+  
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Main Stats")
+    FGameplayAttributeData Health;
+    ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);
+  
+    UFUNCTION()
+    void OnRep_MaxHealth (const FGameplayAttributeData& OldMaxHealth) const;
+    UFUNCTION()
+    void OnRep_Health (const FGameplayAttributeData& OldHealth) const;
+
+    /* Mana */
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Main Stats")
+    FGameplayAttributeData MaxMana;
+    ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);
+  
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Main Stats")
+    FGameplayAttributeData Mana;
+    ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
+
+    UFUNCTION()
+    void OnRep_MaxMana (const FGameplayAttributeData& OldMaxMana) const;
+    UFUNCTION()
+    void OnRep_Mana (const FGameplayAttributeData& OldMana) const;
 };
