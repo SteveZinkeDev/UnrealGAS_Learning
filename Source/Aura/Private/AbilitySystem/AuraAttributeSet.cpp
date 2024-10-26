10a1bce603e83c3573334c907f07cd904695e648
Steve Zinke <70287923+SteveZinkeDev@users.noreply.github.com>
2024-10-26 16:56:04 +0200

    Created GameplayEffects (instant, duration, infinite) and apply/remove logic + level with a curve table (potions only right now)

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,6 +2,10 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "GameplayEffectExtension.h"
+#include "GameFramework/Character.h"
 #include "Net/UnrealNetwork.h"
 
 UAuraAttributeSet::UAuraAttributeSet ()
@@ -20,6 +24,78 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps (TArray<FLifetimeProperty>& O
   DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
   DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
   DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::PreAttributeChange (const FGameplayAttribute& Attribute, float& NewValue)
+{
+  Super::PreAttributeChange(Attribute, NewValue);
+
+  NewValue = FMath::Max(0.f, NewValue);
+
+  if (Attribute == GetHealthAttribute())
+  {
+    NewValue = FMath::Min(GetMaxHealth(), NewValue);
+  }
+  else if (Attribute == GetManaAttribute())
+  {
+    NewValue = FMath::Min(GetMaxMana(), NewValue);
+  }
+}
+
+void UAuraAttributeSet::PreAttributeBaseChange (const FGameplayAttribute& Attribute, float& NewValue) const
+{
+  Super::PreAttributeBaseChange(Attribute, NewValue);
+
+  NewValue = FMath::Max(0.f, NewValue);
+
+  if (Attribute == GetHealthAttribute())
+  {
+    NewValue = FMath::Min(GetMaxHealth(), NewValue);
+  }
+  else if (Attribute == GetManaAttribute())
+  {
+    NewValue = FMath::Min(GetMaxMana(), NewValue);
+  }
+}
+
+void UAuraAttributeSet::PostGameplayEffectExecute (const struct FGameplayEffectModCallbackData& Data)
+{
+  Super::PostGameplayEffectExecute(Data);
+
+  FEffectProperties Props;
+  SetEffectProperties(Data, Props);
+}
+
+void UAuraAttributeSet::SetEffectProperties (const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const
+{
+  // Source = causer of the effect | Target = target of the effect (owner of this AS)
+
+  Props.EffectContextHandle = Data.EffectSpec.GetContext();
+  Props.SourceASC = Props.EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
+
+  if (IsValid(Props.SourceASC))
+  {
+    Props.SourceAvatarActor = Props.SourceASC->GetAvatarActor();
+    Props.SourceController = Props.SourceASC->AbilityActorInfo->PlayerController.Get();
+    
+    if (Props.SourceController == nullptr && Props.SourceAvatarActor != nullptr)
+    {
+      if (APawn* Pawn = Cast<APawn>(Props.SourceAvatarActor))
+      {
+        Props.SourceController = Pawn->GetController();
+      }
+    }
+
+    if (Props.SourceController)
+    {
+      Props.SourceCharacter = Cast<ACharacter>(Props.SourceController->GetPawn());
+    }
+  }
+
+  Props.TargetAvatarActor = Data.Target.GetAvatarActor();
+  Props.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+  Props.TargetCharacter = Cast<ACharacter>(Props.TargetAvatarActor);
+  Props.TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Props.TargetAvatarActor);
 }
 
 void UAuraAttributeSet::OnRep_MaxHealth (const FGameplayAttributeData& OldMaxHealth) const
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
 #include "AbilitySystemComponent.h"
+#include "GameplayEffectExtension.h"
 #include "AuraAttributeSet.generated.h"
 
 #define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
@@ -12,6 +13,37 @@
         GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
         GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
         GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+
+USTRUCT()
+struct FEffectProperties
+{
+  GENERATED_BODY()
+
+  FEffectProperties ()
+  {
+  }
+
+  FGameplayEffectContextHandle EffectContextHandle;
+
+  UPROPERTY()
+  UAbilitySystemComponent* SourceASC = nullptr;
+  UPROPERTY()
+  AActor* SourceAvatarActor = nullptr;
+  UPROPERTY()
+  AController* SourceController = nullptr;
+  UPROPERTY()
+  ACharacter* SourceCharacter = nullptr;
+
+  UPROPERTY()
+  UAbilitySystemComponent* TargetASC = nullptr;
+  UPROPERTY()
+  AActor* TargetAvatarActor = nullptr;
+  UPROPERTY()
+  AController* TargetController = nullptr;
+  UPROPERTY()
+  ACharacter* TargetCharacter = nullptr;
+};
 
 /**
  * 
@@ -25,16 +57,19 @@ class AURA_API UAuraAttributeSet : public UAttributeSet
     UAuraAttributeSet ();
 
     virtual void GetLifetimeReplicatedProps (TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+    virtual void PreAttributeChange (const FGameplayAttribute& Attribute, float& NewValue) override;
+    virtual void PreAttributeBaseChange (const FGameplayAttribute& Attribute, float& NewValue) const override;
+    virtual void PostGameplayEffectExecute (const struct FGameplayEffectModCallbackData& Data) override;
 
     /* Health */
     UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Main Stats")
     FGameplayAttributeData MaxHealth;
     ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth);
-  
+
     UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Main Stats")
     FGameplayAttributeData Health;
     ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);
-  
+
     UFUNCTION()
     void OnRep_MaxHealth (const FGameplayAttributeData& OldMaxHealth) const;
     UFUNCTION()
@@ -44,7 +79,7 @@ class AURA_API UAuraAttributeSet : public UAttributeSet
     UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Main Stats")
     FGameplayAttributeData MaxMana;
     ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);
-  
+
     UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Main Stats")
     FGameplayAttributeData Mana;
     ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
@@ -53,4 +88,7 @@ class AURA_API UAuraAttributeSet : public UAttributeSet
     void OnRep_MaxMana (const FGameplayAttributeData& OldMaxMana) const;
     UFUNCTION()
     void OnRep_Mana (const FGameplayAttributeData& OldMana) const;
+
+  private:
+    void SetEffectProperties (const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const;
 };
